d62184e48447096028d6603287d8cb023126dfd8
nadure <goglkms@naver.com>
2022-12-16 19:58:41 +0900

    added palyer move

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ pygame.init()
 BASE_DIR = os.getcwd()
 ASSET_DIR = os.path.join(BASE_DIR, "assets")
 
+clock = pygame.time.Clock()
+
 # Set up the drawing window
 screen = pygame.display.set_mode([500, 500])
 
@@ -25,7 +27,7 @@ class EventCheck(object):
 
 
 class PlayerObject(pygame.sprite.Sprite):
-    def __init__(self):
+    def __init__(self, screen_width=500, screen_height=500):
         super(PlayerObject, self).__init__()
         player_picture_path = os.path.join(ASSET_DIR, "player", "player.png")
         picture = pygame.image.load(player_picture_path)
@@ -35,6 +37,38 @@ class PlayerObject(pygame.sprite.Sprite):
 
         # rect from surf by .get_rect()
         self.rect = self.surf.get_rect()
+
+        self.screen_width = screen_width
+        self.screen_height = screen_height
+
+    def update(self):
+        pressed_keys = pygame.key.get_pressed()
+
+        if pressed_keys[K_UP]:
+            self.rect.move_ip(0,-10)
+        
+        if pressed_keys[K_DOWN]:
+            self.rect.move_ip(0,10)
+
+        if pressed_keys[K_LEFT]:
+            self.rect.move_ip(-10,0)
+        
+        if pressed_keys[K_RIGHT]:
+            self.rect.move_ip(10,0)
+
+        if self.rect.left < 0:
+            self.rect.left = 0
+
+        # if self.rect.right > self.screen_width:
+        #     self.rect.right = self.screen_width
+
+        if self.rect.top <= 0:
+            self.rect.top = 0
+        
+        # if self.rect.bottom >= self.screen_height:
+        #     self.rect.bottom = self.screen_height
+
+
         
 Player = PlayerObject()
 # Run until the user asks to quit
@@ -45,10 +79,13 @@ while EVENT.running:
 
     # Fill the background with white
     screen.fill((255, 255, 255))
+    
     screen.blit(Player.surf, Player.rect)
+    Player.update()
 
     # Flip the display
     pygame.display.flip()
+    clock.tick(60)
 
 # Done! Time to quit.
 pygame.quit()
