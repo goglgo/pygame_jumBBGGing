2ccf6a885e29f5c823a15c90d177e68d1689609c
nadure <goglkms@naver.com>
2022-12-18 16:21:12 +0900

    character erected

---
+++ environ_setup.bat
@@ -0,0 +1,4 @@
+python -m venv myvenv
+myvenv\scripts\activate.bat
+python -m pip install -U pip
+pip install -r requirements.txt
--- main.py
+++ main.py
@@ -54,6 +54,17 @@ class PlayerObject(pygame.sprite.Sprite):
         self.screen_height = int(os.environ.get('SCREEN_HEIGHT', 500))
 
         self.player_y_momentum = 0.2
+        self.collide = {
+            "left":False,
+            "right":False,
+            "up":False,
+            "bottom":False,
+        }
+
+    def move_check(self, collison):
+        # print(collison.rect)
+        self.collide["bottom"] = True
+        # print(self.collide["bottom"])
 
     def update(self):
         pressed_keys = pygame.key.get_pressed()        
@@ -70,13 +81,30 @@ class PlayerObject(pygame.sprite.Sprite):
         if self.rect.right > self.screen_width:
             self.rect.right = self.screen_width
 
-        if self.rect.y > self.screen_height - self.picture.get_height():
-            self.player_y_momentum = -self.player_y_momentum
+        if(self.collide["bottom"] == False):
+            if self.rect.y > self.screen_height - self.picture.get_height():
+                self.player_y_momentum = -self.player_y_momentum
+            
+            else:
+                self.player_y_momentum += 0.2
         
+            self.rect.move_ip(0, self.player_y_momentum)
+            # print(self.collide["bottom"])
         else:
-            self.player_y_momentum += 0.2
+            self.rect.move_ip(0,0)
+
+class GameTile(pygame.sprite.Sprite):
+    def __init__(self, img_path, img_size_x, img_size_y):
+        super(GameTile, self).__init__()
+        img = pygame.image.load(img_path).convert()
+        self.surf = pygame.transform.scale(img, (img_size_x, img_size_y))
+        self.img_size_x = img_size_x
+        self.img_size_y = img_size_y
+    
+    def set_rect(self, x, y):
+        self.rect = pygame.Rect(x, y, self.img_size_x, self.img_size_y)
         
-        self.rect.move_ip(0, self.player_y_momentum)
+        return self
 
 
 class CollideTest(pygame.sprite.Sprite):
@@ -95,14 +123,16 @@ class CollideTest(pygame.sprite.Sprite):
 
 class GameMap(object):
     def __init__(self):
-        dirt_path = os.path.join(ASSET_DIR, "tiles", "dirt.png")
-        grass_path = os.path.join(ASSET_DIR, "tiles", "grass.png")
+        self.dirt_path = os.path.join(ASSET_DIR, "tiles", "dirt.png")
+        self.grass_path = os.path.join(ASSET_DIR, "tiles", "grass.png")
+
+        self.screen_width = int(os.environ.get('SCREEN_WIDTH', 500))
+        self.screen_height = int(os.environ.get('SCREEN_HEIGHT', 500))
+        self.surf = pygame.Surface((self.screen_width, self.screen_height))
         
-        self.tile_size = 25
-        self.dirt_img = pygame.image.load(dirt_path)
-        self.grass_img = pygame.image.load(grass_path)
-        self.none_img = pygame.Surface((self.tile_size, self.tile_size))
-        self.none_img.fill((82, 89, 93))
+        # self.dirt_img = pygame.image.load(dirt_path).convert()
+        # self.grass_img = pygame.image.load(grass_path).convert()
+        
         self.game_map = [   [0,0,0,0,0,0,0,0,0,0,0,0],
                             [0,0,0,0,0,0,0,0,0,0,0,0],
                             [0,0,0,0,0,0,0,0,0,0,0,0],
@@ -116,18 +146,37 @@ class GameMap(object):
                             [2,2,2,2,2,2,2,2,2,2,2,2],
                             [2,2,2,2,2,2,2,2,2,2,2,2],   ]
 
+        self.tile_size_x = self.screen_width // len(self.game_map)
+        self.tile_size_y = self.screen_height // len(self.game_map[0])
+        self.none_img = pygame.Surface((self.tile_size_x, self.tile_size_y)).convert()
+        self.none_img.fill((82, 89, 93))
+
+        # self.dirt_img = GameTile(dirt_path, self.tile_size_x, self.tile_size_y)
+        # self.grass_img = GameTile(grass_path, self.tile_size_x, self.tile_size_y)
+
+        # self.dirt_img = pygame.transform.scale(self.dirt_img, (self.tile_size_x, self.tile_size_y))
+        # self.grass_img = pygame.transform.scale(self.grass_img, (self.tile_size_x, self.tile_size_y))
+
+        self.collide_group = pygame.sprite.Group()
     
-    def update(self, surf : pygame.Surface):
+    def update(self):
         y = 0
         for row in self.game_map:
             x = 0
             for tile in row:
                 if tile == 0 :
-                    surf.blit(self.none_img, (x * self.tile_size, y * self.tile_size))
+                    self.surf.blit(self.none_img, (x * self.tile_size_x, y * self.tile_size_y))
+
                 if tile == 1 :
-                    surf.blit(self.grass_img, (x * self.tile_size, y * self.tile_size))
+                    grass_img = GameTile(self.grass_path, self.tile_size_x, self.tile_size_y)
+                    grass_img.set_rect(x * self.tile_size_x, y * self.tile_size_y)
+                    self.surf.blit(grass_img.surf, grass_img.rect)
+                    self.collide_group.add(grass_img)
+                    
                 if tile == 2 :
-                    surf.blit(self.dirt_img, (x * self.tile_size, y * self.tile_size))
+                    dirt_img = GameTile(self.dirt_path, self.tile_size_x, self.tile_size_y)
+                    dirt_img.set_rect(x * self.tile_size_x, y * self.tile_size_y)
+                    self.surf.blit(dirt_img.surf, dirt_img.rect)
                 x += 1
             y += 1
         
@@ -149,28 +198,25 @@ EVENT = EventCheck()
 while EVENT.running:
     EVENT.check()
 
-    # screen에 흰색을 채웁니다.
-    screen.fill((255, 255, 255))
+    Map.update()
+    background = pygame.transform.scale(Map.surf, (SCREEN_WIDTH, SCREEN_HEIGHT))
     
-    # display에 다른 색을 채웁니다.
-    display.fill((82, 89, 93))
-    display.blit(Player.surf, Player.rect)
-    display.blit(test_sprite.surf, test_sprite.rect)
-    
-    # display를 screen (0,0) 좌표에 뿌립니다.
-    display = pygame.transform.scale(display, (SCREEN_WIDTH, SCREEN_HEIGHT))
-    Map.update(display)
-    
-    screen.blit(display, (0, 0))
-
     Player.update()
 
+    background.blit(Player.surf, Player.rect)
+    background.blit(test_sprite.surf, test_sprite.rect)
+    screen.blit(background, (0, 0))
+    
     # collide check
     if pygame.sprite.spritecollideany(Player, collide_sprites):
         test_sprite.red_update()
     
     else:
         test_sprite.black_update()
+    
+    collide_with_map = pygame.sprite.spritecollideany(Player, Map.collide_group)
+    if collide_with_map:
+        Player.move_check(collide_with_map)
 
     # Flip the display
     pygame.display.flip()
